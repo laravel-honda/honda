68504e44783b2b384d677b3c627dbe3171a3372f
Félix Dorn <github@felixdorn.fr>
2021-05-03 00:51:32 +0200

    Revert "fix"
    This reverts commit 1096325e60a78f0bc108d869a19b951883cc0607.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,1 @@
 require('alpinejs')
-
-class Wire {
-    watching = {};
-
-    watch(name) {
-        const element = document.querySelector('input[name="' + name + '"]')
-        element.addEventListener('input', (e) => {
-
-        })
-        return this
-    }
-
-
-}
-
-window.Wire = new Wire()
