1920e41e165f2f7d2b1c318a85790896f484249d
Félix Dorn <github@felixdorn.fr>
2021-05-22 17:42:07 +0200

    wip

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,19 +8,19 @@
     <title>{{ $title }} | {{ config('app.name') }}</title>
     <meta property="og:type" content="website">
     <meta property="og:title" content="{{ $title }} | {{ config('app.name') }}"/>
-    @if ($description)
+    @isset ($description)
         <meta name="description" content="{{ $description }}">
         <meta property="og:description" content="{{ $description }}">
-    @endif
+    @endisset
     <meta property="og:url" content="{{ request()->url() }}"/>
     <meta property="og:locale" content="{{ App::getLocale() }}"/>
     <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}">
     @livewireStyles
     <x-assets-style href="css/app.css"/>
     <x-assets-script src="js/app.js"/>
-    <x-assets-resources/>
+    <x-assets-render/>
 </head>
-<body {{ $attributes }}>
+<body {{ $attributes->class('bg-gray-100 font-medium') }}>
 {{ $slot }}
 @livewireScripts
 </body>
