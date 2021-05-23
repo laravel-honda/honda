91a8b425e0606f29bfb7b5b04513803b34090997
Félix Dorn <github@felixdorn.fr>
2021-05-23 14:50:43 +0200

    wip

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,9 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -13,8 +16,7 @@ class Handler extends ExceptionHandler
      *
      * @var array
      */
-    protected $dontReport = [
-    ];
+    protected $dontReport = [];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
@@ -28,10 +30,53 @@ class Handler extends ExceptionHandler
 
     public function report(Throwable $exception)
     {
-        if ($this->shouldReport($exception) && app()->bound('sentry')) {
-            app('sentry')->captureException($exception);
+        if ($this->shouldReport($exception)) {
+            $response = Http::post('http://localhost:8000/api/capture', $this->makePayload($exception));
+
+            dd($response->body());
         }
 
         parent::report($exception);
+    }
+
+    public function makePayload(Throwable $throwable): array
+    {
+        return [
+            'token' => 'proj_2o2aTbrecmbBpjleGrcJys5kGdFfMeYGYURxR7j.60aa059356d1c0.0718',
+            'title' => $throwable->getMessage(),
+            'context' => [
+                'trace' => $this->getTrace($throwable),
+                'request' => [
+                    'env' => app()->environment(),
+                    'ip' => request()->ip(),
+                    'agent' => request()->userAgent(),
+                    'headers' => Arr::except(request()->headers->all(), ['authorization'])
+                ],
+                'user_id' => Auth::id()
+            ]
+        ];
+    }
+
+    public function getTrace(array $rawTrace): array
+    {
+        $trace = [];
+
+        foreach ($rawTrace as $call) {
+            $trace[] = array_merge($call, [
+                'file' => str_replace(base_path(), '', $call['file']),
+                'code' => $this->getCodeForLine($call['file'], $call['line'])
+            ]);
+        }
+
+        return $trace;
+    }
+
+    public function getCodeForLine(string $file, int $line): string {
+        $contents = file_get_contents($file);
+        $lines = explode(PHP_EOL, $contents);
+
+        return implode(PHP_EOL, array_filter($lines, function ($key) use ($line) {
+            return $key <= $line + 5 && $key >= $line - 5 ;
+        }, ARRAY_FILTER_USE_KEY));
     }
 }
